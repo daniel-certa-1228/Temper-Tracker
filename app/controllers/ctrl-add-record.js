14764c8f70ead3b9813143ec7a14fd82fbd2e8d3
daniel-certa-1228 <mfdancerta@gmail.com>
2017-09-10 23:02:31 -0500

    Pushing record object

--- app/controllers/ctrl-add-record.js
+++ app/controllers/ctrl-add-record.js
@@ -1,5 +1,28 @@
 "use strict";
 
-app.controller('AddRecordCtrl', function($scope, $location, $routeParams) {
+app.controller('AddRecordCtrl', function($scope, $location, $routeParams, UserFactory, RecordFactory, ChildFactory) {
+
+	let user = UserFactory.getCurrentUser();
+
+	$scope.record = {
+		childID:'',
+		date:'',
+		time:'',
+		duration:'',
+		antecedent:'',
+		consequence:'',
+		comments:'',
+		uid: user
+	};
+
+	$scope.addRecord = () => {
+		console.log( "$scope.record", $scope.record );
+		RecordFactory.addRecord($scope.record)
+		.then((data) => {
+			console.log( "data from AddRecordCtrl", data );
+			$location.url('/list-records');
+		});
+	};
+
 
 });
