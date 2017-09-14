f6cce042f9e611553d026c8377ba938310140bb0
daniel-certa-1228 <mfdancerta@gmail.com>
2017-09-14 15:20:50 -0500

    4 graphs working ona per-child basis

--- app/controllers/ctrl-graph-child.js
+++ app/controllers/ctrl-graph-child.js
@@ -188,6 +188,136 @@ app.controller("RadarChildCtrl", function ($scope, $routeParams, UserFactory, Re
   getDurationData();
 });
 
+///////////////////////////////////////////////////////
+//////////////// TIME OF DAY LINE GRAPH  //////////////
+///////////////////////////////////////////////////////
+
+app.controller("LineChildCtrl", function ($scope, $routeParams, UserFactory, RecordFactory) {
+
+  $scope.labels = ["6am", "7am", "8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm"];
+  $scope.series = ['Series A'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 55, 40]
+  ];
+
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+
+  let user = UserFactory.getCurrentUser();
+
+  $scope.TimeArray = [];
+
+  $scope.SixAmArray = [];
+  $scope.SevenAmArray = [];
+  $scope.EightAmArray = [];
+  $scope.NineAmArray = [];
+  $scope.TenAmArray = [];
+  $scope.ElevenAmArray = [];
+  $scope.TwelvePmArray = [];
+  $scope.OnePmArray = [];
+  $scope.TwoPmArray = [];
+  $scope.ThreePmArray = [];
+  $scope.FourPmArray = [];
+  $scope.FivePmArray = [];
+  $scope.SixPmArray = [];
+  $scope.SevenPmArray = [];
+  $scope.EightPmArray = [];
+  $scope.NinePmArray = [];
+
+  const getTimeData = () => {
+  	RecordFactory.getRecordsByChildID($routeParams.itemId)
+  	.then((data) => {
+  		// console.log( "time data", data );
+  		data.forEach((record) => {
+  			let fixedTime = new Date(record.time);
+  			$scope.TimeArray.push(fixedTime.toString());
+  		});
+  		return $scope.TimeArray;
+  	})
+  	.then((times) => {
+  		// console.log( "TIMES", times );
+  		times.forEach((time) =>{
+
+  			if ( (/([0][6]:)([0-2][0-9]:)/ig).test(time) ) {
+  				$scope.SixAmArray.push(time);
+  			}
+
+  			if ( /([0][6]:)([3-5][0-9]:)/ig.test(time) || /([0][7]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.SevenAmArray.push(time);
+  			}
+  			if ( /([0][7]:)([3-5][0-9]:)/ig.test(time) || /([0][8]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.EightAmArray.push(time);
+  			}
+  			if ( /([0][8]:)([3-5][0-9]:)/ig.test(time) || /([0][9]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.NineAmArray.push(time);
+  			}
+  			if ( /([0][9]:)([3-5][0-9]:)/ig.test(time) || /([1][0]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.TenAmArray.push(time);
+  			}
+  			if ( /([1][0]:)([3-5][0-9]:)/ig.test(time) || /([1][1]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.ElevenAmArray.push(time);
+  			}
+  			if ( /([1][1]:)([3-5][0-9]:)/ig.test(time) || /([1][2]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.TwelvePmArray.push(time);
+  			}
+  			if ( /([1][2]:)([3-5][0-9]:)/ig.test(time) || /([1][3]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.OnePmArray.push(time);
+  			}
+  			if ( /([1][3]:)([3-5][0-9]:)/ig.test(time) || /([1][4]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.TwoPmArray.push(time);
+  			}
+  			if ( /([1][4]:)([3-5][0-9]:)/ig.test(time) || /([1][5]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.ThreePmArray.push(time);
+  			}
+  			if ( /([1][5]:)([3-5][0-9]:)/ig.test(time) || /([1][6]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.FourPmArray.push(time);
+  			}
+  			if ( /([1][6]:)([3-5][0-9]:)/ig.test(time) || /([1][7]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.FivePmArray.push(time);
+  			}
+  			if ( /([1][7]:)([3-5][0-9]:)/ig.test(time) || /([1][8]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.SixPmArray.push(time);
+  			}
+  			if ( /([1][8]:)([3-5][0-9]:)/ig.test(time) || /([1][9]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.SevenPmArray.push(time);
+  			}
+  			if ( /([1][9]:)([3-5][0-9]:)/ig.test(time) || /([2][0]:)([0-2][0-9]:)/ig.test(time) ) {
+  				$scope.EightPmArray.push(time);
+  			}
+  			if ( /([2][0]:)([3-5][0-9]:)/ig.test(time) || /([2][1]:)([0-5][0-9]:)/ig.test(time) ) {
+  				$scope.NinePmArray.push(time);
+  			}
+  		});
+  		 
+  	})
+  	.then(() => {
+  		$scope.data[0] = [$scope.SixAmArray.length, $scope.SevenAmArray.length, $scope.EightAmArray.length, $scope.NineAmArray.length, $scope.TenAmArray.length, $scope.ElevenAmArray.length, $scope.TwelvePmArray.length, $scope.OnePmArray.length, $scope.TwoPmArray.length, $scope.ThreePmArray.length, $scope.FourPmArray.length, $scope.FivePmArray.length, $scope.SixPmArray.length, $scope.SevenPmArray.length, $scope.EightPmArray.length, $scope.NinePmArray.length];
+  		$scope.numbers1 = $scope.data[0].slice(0,7);
+  		$scope.numbers2 = $scope.data[0].slice(8,15);
+  		$scope.labels1 = $scope.labels.slice(0,7);
+  		$scope.labels2 = $scope.labels.slice(8,15);
+  	});
+  };
+  getTimeData();
+});
+
 // app.controller('BarCtrl', ['$scope', function ($scope) {
 //     $scope.options = { legend: { display: true } };
 //     $scope.labels = ['0-4 min.', '5-10 min.', '11-20 min.', '21-30 min.', 'Over 30 min.'];
