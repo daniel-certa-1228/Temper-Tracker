b35d386141915a19028a94ad72515107f81be00e
daniel-certa-1228 <mfdancerta@gmail.com>
2017-10-12 22:17:08 -0500

    remove logs from dist

--- dist/built.js
+++ dist/built.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log( "app.js" );
+// console.log( "app.js" );
 
 const app = angular.module('TemperTracker', ['ngRoute', 'chart.js', 'ngAnimate']);
 
@@ -1704,7 +1704,7 @@ app.controller('LoginCtrl', function($scope, $location, $routeParams, UserFactor
 				$location.path('/home');
 				$scope.$apply();
 			}
-			console.log( "login successful" );
+			// console.log( "login successful" );
 		})
 		.catch((error) => {
 			console.log( "login error", error );
@@ -1737,11 +1737,11 @@ app.controller('NavbarCtrl', function($scope, $location, $routeParams, $window, 
   	$scope.tourMode_two = false;
   	// handles logout button functions
 	$scope.logOut = () => {
-		console.log( "logout firing" );
+		// console.log( "logout firing" );
 		UserFactory.logOut()
 		.then(() => {
 			let user = UserFactory.getCurrentUser();
-			console.log("logOut successful", user);
+			// console.log("logOut successful", user);
 		})
 		.then(()=> {
 			$window.location.href = "#!/";
@@ -1754,13 +1754,13 @@ app.controller('NavbarCtrl', function($scope, $location, $routeParams, $window, 
 	firebase.auth().onAuthStateChanged(function(user) {
 	    if (user) {
 	      $scope.isLoggedIn = true;
-	      console.log("currentUser logged in?", user);
-	      console.log("logged in t-f", $scope.isLoggedIn );
+	      // console.log("currentUser logged in?", user);
+	      // console.log("logged in t-f", $scope.isLoggedIn );
 	      $scope.userPhoto = user.photoURL;
 	      $scope.$apply();
 	    } else {
 	      $scope.isLoggedIn = false;
-	      console.log("user logged in?", $scope.isLoggedIn);
+	      // console.log("user logged in?", $scope.isLoggedIn);
 	      $window.location.href = "#!/login";
     	}
   	});
